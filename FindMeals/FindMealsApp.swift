02afc2543b762eb58888d23fa1af10c3f4626de1
admin <admin>
2026-08-19 12:22:50 +0800

    Initial Commit

---
+++ FindMeals/FindMealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMealsApp.swift
+//  FindMeals
+//
+//  Created by Revan Arturito on 19/08/26.
+//
+
+import SwiftUI
+
+@main
+struct FindMealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
