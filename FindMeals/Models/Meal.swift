b2e257649320d774f937727165e389c484f92efe
admin <admin>
2026-08-19 15:46:07 +0800

    feat: Meal Service

--- FindMeals/Models/Meal.swift
+++ FindMeals/Models/Meal.swift
@@ -5,4 +5,24 @@
 //  Created by Revan Arturito on 19/08/26.
 //
 
+import Foundation
 
+struct Meal: Codable {
+    let id: String
+    let name: String
+    let imageURL: String
+    let area: String?
+    let country: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "idMeal"
+        case name = "strMeal"
+        case imageURL = "strMealThumb"
+        case area = "strArea"
+        case country = "strCountry"
+    }
+}
+
+struct MealResponse: Codable {
+    let meals: [Meal]
+}
